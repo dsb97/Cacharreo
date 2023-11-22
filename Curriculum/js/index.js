08d69a5a6a9d48fa0435f59c6bdf72af2169e8aa
davidsanchezbarragan <dsancbar@emeal.nttdata.com>
2023-11-22 15:07:39 +0100

    UNDER CONSTRUCTION COMMIT

--- Curriculum/js/index.js
+++ Curriculum/js/index.js
@@ -1,7 +1,253 @@
+var taskManager = class taskManager {
+  constructor(windowID) {
+    this.xValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60];
+    this.yValuesCPU = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.yValuesMemory = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.previousCPUValue = 0;
+    this.CPUUsageChart = null;
+    this.parentWindow = document.getElementById(windowID);
+    this.parentWindowDocument = this.parentWindow.getElementsByTagName('iframe')[0].contentDocument;
+    debugger;
+    this.windowTitle = this.parentWindow.getElementsByTagName('div')[3];
+    this.backButton = this.parentWindow.getElementsByClassName('backButton')[0];
+    this.forwardButton = this.parentWindow.getElementsByClassName('forwardButton')[0];
+    this.refreshButton = this.parentWindow.getElementsByClassName('refreshButton')[0];
+    this.stopButton = this.parentWindow.getElementsByClassName('stopButton')[0];
+    this.homeButton = this.parentWindow.getElementsByClassName('homeButton')[0];
+    this.backButton.onclick = () => back();
+    this.forwardButton.onclick = () => forward();
+    this.refreshButton.onclick = () => refresh();
+    this.stopButton.onclick = () => stop();
+    this.homeButton.onclick = () => home();
+    this.load();
+  }
+
+  toggleCheckRows(event) {
+    let ids = document.querySelectorAll('.checkedApps');
+
+    for (let i = 0; i < ids.length; i++) {
+      ids[i].checked = event.target.checked;
+    }
+  }
+
+  loadCPU() {
+    // Get CPU usage and update the CPU usage display
+    if (window.performance && window.performance.now) {
+      var cpuUsage = window.performance.now();
+      this.parentWindowDocument.querySelector("#cpu-usage").textContent = `Uso de CPU: ${((cpuUsage - this.previousCPUValue) / 50).toFixed(2)} %`;
+      //this.addData((cpuUsage - this.previousCPUValue) / 50, 0);
+      this.previousCPUValue = cpuUsage;
+    }
+
+    this.parentWindowDocument.querySelector('#processor-number').textContent = `Procesadores lógicos: ${navigator.hardwareConcurrency}`;
+
+    // Get memory usage and update the memory usage display
+    if (window.performance && window.performance.memory) {
+      var memoryUsage = window.performance.memory.usedJSHeapSize / (1024 * 1024 * 1024);
+      this.parentWindowDocument.querySelector("#memory-usage").textContent = `Memoria en uso: ${(memoryUsage).toFixed(2)} GB`;
+      //this.addData(memoryUsage, 1);
+    }
+  }
+
+  loadTasks() {
+    let tasks = getCachedWindows();
+    this.parentWindowDocument.querySelector('#processesCount').innerHTML = '&nbsp;' + tasks.length;
+    let openApps = [...new Set(tasks.map(item => item.dockIcon))];
+    let windows = '';
+    let group = `
+  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+      <li>
+          <table class="table table-striped table-hover">
+              <thead>
+                  <tr>
+                      <th class="defaultFontColor" scope="col"><input class="form-check-input" type="checkbox" onchange="toggleCheckRows(event)" ></th>
+                      <th class="defaultFontColor" scope="col">&nbsp</th>
+                      <th class="defaultFontColor" scope="col">ID proceso</th>
+                      <th class="defaultFontColor" scope="col">Nombre</th>
+                      <th class="defaultFontColor" scope="col">Memoria</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  replaceContent
+              </tbody>
+          </table>
+      </li>
+  </ul>
+                  `;
+    let tareas = this.parentWindowDocument.querySelector('#activities-collapse');
+    tareas.innerHTML = "";
+    openApps.forEach((app) => {
+      tasks.forEach((x) => {
+        if (x.dockIcon == app) {
+          let systemApp = window.parent.systemApps.content.filter((x) => { if (x.internalName == `${app}.app`) { return x } })[0];
+          windows += `
+  <tr>
+      <th class="defaultFontColor" scope="row"><input class="form-check-input checkedApps" type="checkbox" value="${x.windowID}" ></th>
+      <td class="defaultFontColor"><img src="${systemApp.icon}" style="height: 12pt; width: 12pt;"/></td>
+      <td class="defaultFontColor">${x.windowID}</td>
+      <td class="defaultFontColor">${systemApp.name}</td>
+      <td class="defaultFontColor">${new Blob([JSON.stringify(x.history.urlList)]).size} bytes</td>
+  </tr>
+                          `;
+        }
+      });
+    });
+    group = group.replaceAll('replaceContent', windows);
+    tareas.innerHTML += group;
+  }
+
+  closeApp(windowID) {
+    let cW = getCachedWindow(windowID);
+    //Remove from cache
+    removeCachedWindow(windowID);
+
+    //Query cache to remove or not dock indicator
+    let cWs = getCachedWindows();
+    let openWindows = cWs.filter((obj) => {
+      return obj.dockIcon == cW.dockIcon;
+    });
+    if (openWindows.length == 0) {
+      document.getElementById(cW.dockIcon).classList.remove('li-on');
+    }
+
+    //Undraw the window
+    document.getElementById(windowID).remove();
+  }
+
+  addData(newValue, datasetIndex) {
+    debugger;
+    let newData = this.CPUUsageChart.data.datasets[datasetIndex].data;
+    if (newData.length >= 60) {
+      newData = newData.slice(1, 60);
+    }
+    newData.push(newValue);
+    this.CPUUsageChart.data.datasets[datasetIndex].data = newData;
+    this.CPUUsageChart.update();
+  }
+
+  loadCPUGraph() {
+    this.CPUUsageChart = new Chart("CPUUsage", {
+      type: "line",
+      data: {
+        labels: this.xValues,
+        datasets: [
+          {
+            label: 'Uso de CPU',
+            fill: true,
+            lineTension: 0,
+            backgroundColor: "rgba(95,171,126,0.8)",
+            borderColor: "rgba(95,171,126,1)",
+            data: this.yValuesCPU
+          },
+          {
+            label: 'Uso de memoria',
+            fill: true,
+            lineTension: 0,
+            backgroundColor: "rgba(244,96,93,0.8)",
+            borderColor: "rgba(244,96,93,1)",
+            data: this.yValuesMemory,
+            hidden: true
+          },
+        ]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              color: '#ffffff'
+            }
+          }
+        },
+        dataset: {
+          label: { color: '#ffffff' }
+        },
+        scales: {
+          y: {
+            border: {
+              display: false
+            },
+            grid: {
+              color: getSetting(keySettings.fontColor),
+            },
+            ticks: {
+              color: getSetting(keySettings.fontColor),
+              font: {
+                family: 'Varela Round',
+                size: 9,
+                style: 'normal'
+              }
+            }
+          },
+          x: {
+            border: {
+              display: false
+            },
+            grid: {
+              color: getSetting(keySettings.fontColor),
+            },
+            ticks: {
+              color: getSetting(keySettings.fontColor),
+              font: {
+                family: 'Varela Round',
+                size: 9,
+                style: 'normal'
+              }
+            }
+          }
+        },
+        animation: {
+          duration: 0
+        }
+      }
+    });
+  }
+
+  load() {
+    debugger;
+    this.windowTitle.innerHTML = 'Monitor del sistema';
+    this.backButton.style.display = "none";
+    this.forwardButton.style.display = "none";
+    this.refreshButton.title = "Refrescar tareas";
+    this.stopButton.title = "Cerrar tarea";
+    this.homeButton.style.display = "none";
+
+    this.loadTasks();
+    this.loadCPUGraph();
+    var lCPU = this.loadCPU;
+    //setInterval(function() { lCPU(this.parentWindowDocument); }, 1000);
+    this.loadCPU();
+    showWindow(this.parentWindow);
+  }
+
+  back() {
+
+  }
+
+  forward() {
+
+  }
+
+  refresh() {
+    this.loadTasks();
+  }
+
+  stop() {
+    let ids = document.querySelectorAll('.checkedApps:checked');
+    for (let i = 0; i < ids.length; i++) {
+      this.closeApp(ids[i].value);
+    }
+    this.loadTasks();
+  }
+
+  home() {
+
+  }
+}
 
 window.addEventListener('DOMContentLoaded', function () {
   createWindowsCache();
-  
+
   refresh();
 
 });
@@ -593,7 +839,7 @@ function openWindow(url, dockIcon, path) {
   //Get opened windows.
   //If there is an minimized instance, it will be restored
   //If there is no minimized instance, it will create a new one
-  ;
+  debugger;
   let cWs = getCachedWindows();
   if (cWs.some((element) => element.dockIcon == dockIcon && element.windowStatus == windowStatus.minimized)) {
     cWs.forEach((element) => {
@@ -676,7 +922,7 @@ function restoreMaximizeWindow(event) {
   } else {
     el.classList.add('max');
     el.style.backgroundColor = null;
-    el.style.backgroundColor = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${parseFloat(rgba[3])+0.4})`;
+    el.style.backgroundColor = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${parseFloat(rgba[3]) + 0.4})`;
 
   }
   el.style.zIndex = maxZIndex(el.style.zIndex);
